20e7238840721d01e143329866c4adeaced9e5e4
amersoul <amine.3dsmax@live.fr>
2021-06-25 16:50:36 +0100

    code cleanup

---
+++ swifty-companion/File.swift
@@ -0,0 +1,9 @@
+a//
+//  File.swift
+//  swifty-companion
+//
+//  Created by Amine Mersoul on 6/25/21.
+//  Copyright © 2021 Amine Mersoul. All rights reserved.
+//
+
+import Foundation
---
+++ swifty-companion/Model/Token.swift
@@ -0,0 +1,19 @@
+//
+//  Token.swift
+//  swifty-companion
+//
+//  Created by Amine Mersoul on 6/25/21.
+//  Copyright © 2021 Amine Mersoul. All rights reserved.
+//
+
+import Foundation
+
+struct Token: Decodable {
+    let access_token: String?
+    let token_type: String?
+    let expires_in: Int?
+    let scope: String?
+    let created_at: Int?
+    let error: String?
+    let error_description: String?
+}
